8ec1aa294d306243b12d91c819fce4f61507920a
httech <hieu@tnhlabs.com>
2018-09-11 22:59:54 -0500

    Create model for Goal

---
+++ Daily3Goals/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Daily3Goals
+{
+    public struct Goal
+    {
+        public string Name { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
