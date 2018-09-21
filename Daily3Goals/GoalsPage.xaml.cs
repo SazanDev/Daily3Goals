eb4091d51aa25feeb8083acb98fe5592ed8be100
httech <hieu@tnhlabs.com>
2018-09-20 22:29:55 -0500

    Move saving and loading into repository

--- Daily3Goals/GoalsPage.xaml.cs
+++ Daily3Goals/GoalsPage.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace Daily3Goals
@@ -8,42 +6,22 @@ namespace Daily3Goals
     public partial class GoalsPage : ContentPage
     {
         GoalsViewModel viewModel;
+        GoalRepository goalRepository;
 
         public GoalsPage()
         {
+            goalRepository = new GoalRepository();
+
             InitializeComponent();
 
-            viewModel = new GoalsViewModel(Load(), DateTime.Now);
+            viewModel = new GoalsViewModel(goalRepository.Load(), DateTime.Now);
 
             viewModel.GoalsChanged += (sender, goals) => {
                 // Probably shouldn't save right away on every single edit
-                Save(goals);
+                goalRepository.Save(goals);
             };
 
             BindingContext = viewModel;
-        }
-
-        Goal[] Load()
-        {
-            string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "goals.json");
-            if (File.Exists(fileName)) {
-                Goal[] savedGoals = JsonConvert.DeserializeObject<Goal[]>(File.ReadAllText(fileName));
-                // Only populate goal if it's the same date as today
-                if (savedGoals[0].Date.Year == DateTime.Now.Year
-                    && savedGoals[0].Date.Month == DateTime.Now.Month
-                    && savedGoals[0].Date.Day == DateTime.Now.Day) {
-                    return savedGoals;
-                }
-            }
-
-            return null;
-        }
-
-        void Save(Goal[] goals)
-        {
-            string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "goals.json");
-            string json = JsonConvert.SerializeObject(goals);
-            File.WriteAllText(fileName, json);
         }
     }
 }
