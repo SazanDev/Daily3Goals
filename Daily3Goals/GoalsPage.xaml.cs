d0f836828114cbb29ab8b113b5c3c4a79996ed02
httech <hieu@tnhlabs.com>
2018-09-20 22:11:11 -0500

    Refactor bindings into view model

--- Daily3Goals/GoalsPage.xaml.cs
+++ Daily3Goals/GoalsPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Windows.Input;
 using Newtonsoft.Json;
 using Xamarin.Forms;
 
@@ -9,107 +7,39 @@ namespace Daily3Goals
 {
     public partial class GoalsPage : ContentPage
     {
-        // Our 3 goals
-        List<Goal> goals;
-        // Current date
-        DateTime date;
+        GoalsViewModel viewModel;
 
         public GoalsPage()
         {
-            goals = new List<Goal>();
+            InitializeComponent();
 
-            // All goals start as this
-            Goal goalTemplate = new Goal {
-                Date = DateTime.Now,
-                Name = ""
+            viewModel = new GoalsViewModel(Load(), DateTime.Now);
+
+            viewModel.GoalsChanged += (sender, goals) => {
+                // Probably shouldn't save right away on every single edit
+                Save(goals);
             };
 
-            goals.Add(goalTemplate);
-            goals.Add(goalTemplate);
-            goals.Add(goalTemplate);
-
-            date = DateTime.Now;
-
-            DoneCommand = new Command<int>(index => {
-                // Toggle the done property when the done button is pressed
-                Goal goal = goals[index];
-                goal.Done = !goal.Done;
-                goals[index] = goal;
-                OnPropertyChanged("Goal" + (index + 1) + "Done");
-            });
-
-            // Load goals from file if any
-            Load();
-
-            // Save goal when app goes to background
-            MessagingCenter.Subscribe<App>(this, "OnSleep", sender => {
-                Save();
-            });
-
-            InitializeComponent();
+            BindingContext = viewModel;
         }
 
-        public string Date => date.ToString("dddd, MMMM dd, yyyy");
-
-        public string Goal1 {
-            get => goals[0].Name;
-            set {
-                if (goals[0].Name != value) {
-                    Goal newGoal = goals[0];
-                    newGoal.Name = value;
-                    goals[0] = newGoal;
-                    OnPropertyChanged("Goal1");
-                }
-            }
-        }
-
-        public string Goal2 {
-            get => goals[1].Name;
-            set {
-                if (goals[1].Name != value) {
-                    Goal newGoal = goals[1];
-                    newGoal.Name = value;
-                    goals[1] = newGoal;
-                    OnPropertyChanged("Goal2");
-                }
-            }
-        }
-
-        public string Goal3 {
-            get => goals[2].Name;
-            set {
-                if (goals != null & goals[2].Name != value) {
-                    Goal newGoal = goals[2];
-                    newGoal.Name = value;
-                    goals[2] = newGoal;
-                    OnPropertyChanged("Goal3");
-                }
-            }
-        }
-
-        public bool Goal1Done => goals[0].Done;
-
-        public bool Goal2Done => goals[1].Done;
-
-        public bool Goal3Done => goals[2].Done;
-
-        public ICommand DoneCommand { get; private set; }
-
-        void Load()
+        Goal[] Load()
         {
             string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "goals.json");
             if (File.Exists(fileName)) {
-                List<Goal> savedGoals = JsonConvert.DeserializeObject<List<Goal>>(File.ReadAllText(fileName));
+                Goal[] savedGoals = JsonConvert.DeserializeObject<Goal[]>(File.ReadAllText(fileName));
                 // Only populate goal if it's the same date as today
                 if (savedGoals[0].Date.Year == DateTime.Now.Year
                     && savedGoals[0].Date.Month == DateTime.Now.Month
                     && savedGoals[0].Date.Day == DateTime.Now.Day) {
-                    goals = savedGoals;
+                    return savedGoals;
                 }
             }
+
+            return null;
         }
 
-        void Save()
+        void Save(Goal[] goals)
         {
             string fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "goals.json");
             string json = JsonConvert.SerializeObject(goals);
